7ca80840937ed2a53d598dc657bdcee264edcd39
eja <muhammadfahreza18@gmail.com>
2025-02-03 12:57:19 +0700

    Add files via upload

--- lib/utils/domain.ts
+++ lib/utils/domain.ts
@@ -5,11 +5,6 @@ export function getBaseUrl() {
   }
   
   // Server-side
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-  
-  // Development
   return process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 }
 
